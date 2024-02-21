479bb5a778c75c96093c023e3291c50400ff0fad
Tonyrealzy <umehobiarinze2@gmail.com>
2024-02-21 13:34:36 +0100

    AirBnB_clone_v3 tasks

---
+++ api/v1/app.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+# Create a FLask instance
+app = Flask(__name__)
+
+# Register the blueprint app_views to your Flask instance app
+app.register_blueprint(app_views)
+
+# Declare a method to handle @app.teardown_appcontext that calls storage.close()
+@app.teardown_appcontext
+def teardown_appcontext(error):
+    storage.close()
+
+if __name__ == "__main__":
+    # Run your Flask server (variable app)
+    # Define host and port based on environment variables or defaults
+    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.environ.get('HBNB_API_PORT', 5000))
+
+    # Run the Flask server
+    app.run(host=host, port=port, threaded=True)
