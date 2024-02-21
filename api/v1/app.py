c6b815721b542b5fe151d1692d0433949ee6c5da
Tonyrealzy <umehobiarinze2@gmail.com>
2024-02-21 14:07:30 +0100

    AirBnB_clone_v3 tasks

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 
 
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -13,13 +13,18 @@ app = Flask(__name__)
 # Register the blueprint app_views to your Flask instance app
 app.register_blueprint(app_views)
 
+
 # Declare a method to handle @app.teardown_appcontext that
 # calls storage.close()
-
-
 @app.teardown_appcontext
 def teardown_appcontext(error):
     storage.close()
+    
+
+# Create a handler for 404 errors that returns a JSON-formatted 404 status code response
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
     # Run your Flask server (variable app)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 # Create a route /status on the object app_views
@@ -11,3 +12,19 @@ from api.v1.views import app_views
 def get_status():
     """Return a JSON response"""
     return jsonify({"status": "OK"})
+
+
+# Create an endpoint that retrieves the number
+# of each objects by type
+@app_views.route('/api/v1/stats', methods=['GET'])
+def get_stats():
+    """Return the number of each object by type"""
+    stats = {}
+    classes = ["Amenity", "City", "Place", "Review", "State", "User"]
+
+    for cls_name in classes:
+        cls = storage.classes.get(cls_name)
+        count = storage.count(cls)
+        stats[cls_name] = count
+
+    return jsonify(stats)
