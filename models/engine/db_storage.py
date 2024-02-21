a6e480801c991bfd6cb7701ba9062d411873accf
Tonyrealzy <umehobiarinze2@gmail.com>
2024-02-21 13:45:23 +0100

    AirBnB_clone_v3 tasks

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,8 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+        
+    def get(self, cls, id):
+        """retrieve one object"""
+        if cls and id:
+            return self.__session.query(cls).get(id)
