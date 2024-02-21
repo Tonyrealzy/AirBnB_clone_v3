e3b98d66a6a2cae571e78316e5f20790c276efb0
Tonyrealzy <umehobiarinze2@gmail.com>
2024-02-21 17:29:33 +0100

    AirBnB_clone_v3 tasks

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -13,7 +13,7 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 # and this file (v1/views/__init__.py) won’t be checked.
 
 # Import the views module
-from . import index
+# from . import index
 
 # Import the new file
 from api.v1.views.index import *
@@ -22,6 +22,7 @@ from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
+from api.v1.views.places_reviews import *
 
 
 # Import the new view file
